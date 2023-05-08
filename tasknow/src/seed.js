7f4103becfd9d5577295be5acf6033bcdf9f212e
Gerssonn <gersonhernan00@hotmail.com>
2023-05-08 00:47:12 -0400

    Base inicial

---
+++ tasknow/src/seed.js
@@ -0,0 +1,58 @@
+import shortid from "shortid";
+
+export default store => {
+  console.log("Insert first list");
+  const firstListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: firstListId, listTitle: "Cosas que hacer" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Cocinar"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Ir al medico"
+    }
+  });
+
+  /*
+  console.log("Insert second list");
+  const secondListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: secondListId, listTitle: "Second list" }
+  });
+  
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "Card 1"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "Card 2"
+    }
+  });
+  */
+};
