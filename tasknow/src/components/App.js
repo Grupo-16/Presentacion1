f25555e7d9441f75304b9687aef65e3deb17e23a
Gerssonn <gersonhernan00@hotmail.com>
2023-05-11 19:45:32 -0400

    Fondo estatico

--- tasknow/src/components/App.js
+++ tasknow/src/components/App.js
@@ -10,6 +10,8 @@ class App extends Component {
       backgroundImage:
       'url("https://images2.alphacoders.com/238/thumb-1920-238870.jpg")',
       backgroundSize: "cover",
+      overflowX: "scroll",
+      overflowX: "hidden",
       background: "linear-gradient( rgba(0, 0, 0, 0.5)",
    };
 
