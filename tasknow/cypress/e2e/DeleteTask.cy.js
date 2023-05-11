6184e0ded0a3953374e6c1522279b4249f8deb63
mbarahon <manuel.barahona@usm.cl>
2023-05-10 23:22:47 -0400

    Delete task test

---
+++ tasknow/cypress/e2e/DeleteTask.cy.js
@@ -0,0 +1,19 @@
+describe('Eliminar Tarea', () => {
+    it('Elimina una tarea de la lista', () => {
+        
+        cy.visit('localhost:3000');
+
+        // eliminar el primer elemento de la lista
+        cy.get('.List > .Lists-Cards > .Card')
+            .first()
+            .click()
+            .trigger('mouseover')
+            .get('.Card-Icons')
+            .click()
+            .get('.Button-Delete')
+            .click();
+
+        cy.get('.List > .Lists-Cards > .Card')
+            .should('have.lengthOf', 1);
+    });
+});
